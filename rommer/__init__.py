17eae47d9b173209386661646ccf00d383607d55
Curtis Rueden <ctrueden@wisc.edu>
2020-12-18 14:13:14 -0600

    Recalculate checksums if file has changed

--- rommer/__init__.py
+++ rommer/__init__.py
@@ -73,31 +73,33 @@ def session():
         md5 = Column(String)
         crc = Column(String)
 
+        def stat(self):
+            return pathlib.Path(self.path).stat() if self.path else None
+
+        def is_dirty(self, stat):
+            return self.size is None or self.mtime is None or \
+                   self.size != stat.st_size or self.mtime < stat.st_mtime
+
         def calculate_checksums(self, recalc=False):
-            # TODO: If existing mtime and size, compare path's new values with
-            #       the recorded ones, and recalculate checksums if dirty.
             if not self.path:
-                raise Exception('Cannot calculate metadata without a path')
+                raise Exception('Cannot calculate checksums without a path')
 
-            if recalc:
-                self.size = self.mtime = self.sha1 = self.md5 = self.crc = None
+            stat = self.stat()
+            if recalc or self.is_dirty(stat):
+                self.size = stat.st_size
+                self.mtime = stat.st_mtime
+                self.sha1 = self.md5 = self.crc = None
 
-            if not self.mtime or not self.sha1 or not self.md5 or not self.crc:
+            if not self.sha1 or not self.md5 or not self.crc:
                 # Read file contents; we need to compute something from it.
                 with open(self.path, 'rb') as fh:
                     b = fh.read()
-                if not self.size: self.size = len(b)
+                assert len(b) == self.size
                 if not self.md5: self.md5 = hashlib.md5(b).hexdigest()
                 if not self.sha1: self.sha1 = hashlib.sha1(b).hexdigest()
                 if not self.crc:
                     crc = hex(zlib.crc32(b))[2:]
                     self.crc = '0'*(8-len(crc)) + crc
-
-            if not self.size or not self.mtime:
-                # Get file metadata; we need to remember something from it.
-                stat = pathlib.Path(self.path).stat()
-                if not self.size: self.size = stat.st_size
-                if not self.mtime: self.mtime = stat.st_mtime
 
         def __repr__(self):
             return f"<File(path='{self.path}', size={self.size}, mtime={self.mtime}, sha1={self.sha1}, md5={self.md5}, crc={self.crc})>"
