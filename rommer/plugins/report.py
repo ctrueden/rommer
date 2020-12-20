6016ef28bb1602554001e423a3cd4da1c42c3efa
Curtis Rueden <ctrueden@wisc.edu>
2020-12-19 23:56:07 -0600

    Fix the report command
    Finally back to feature parity with the pre-SQL version! \^_^/

--- rommer/plugins/report.py
+++ rommer/plugins/report.py
@@ -1,4 +1,7 @@
 import logging
+import rommer
+
+from sqlalchemy.orm import joinedload
 
 log = logging.getLogger(__name__)
 
@@ -11,51 +14,54 @@ def configure(parser):
 
 
 def run(args):
-    print(f'run report: {args}')
-    """
-    log.info('Calculating auxiliary data structures')
-    md5s = {}
-    sha1s = {}
-    crcs = {}
-    category_for_game = {}
-    game_for_rom = {}
-    for category, games in dats.items():
-        for game in games:
-            category_for_game[game] = category
-            for rom in game.roms:
-                game_for_rom[rom] = game
-                if rom.md5: md5s[rom.md5] = rom
-                if rom.sha1: sha1s[rom.sha1] = rom
-                if rom.crc: crcs[rom.crc] = rom
+    session = rommer.session()
 
-    log.info('Matching checksums')
+    # TODO: Probably this should be combined with scan.
+    # For scanning, if the file is new, we add it...
+    # And then we do the commit, and finally the big join query.
 
-    matches = {}
-    unmatched = []
+    log.info('Scanning for matches')
+    files = set(str(file.resolve()) for file in rommer.find_files(args.path))
+    matches = session.query(rommer.Rom, rommer.File) \
+                     .filter(rommer.Rom.sha1 == rommer.File.sha1) \
+                     .filter(rommer.Rom.md5 == rommer.File.md5) \
+                     .filter(rommer.Rom.crc == rommer.File.crc) \
+                     .filter(rommer.Rom.size == rommer.File.size)
 
-    for fileinfo in infos_to_analyze:
-        rom = find_rom(fileinfo)
-        if rom:
-            matches[rom] = path
-        else:
-            unmatched.append(path)
+    log.info('Filtering results')
+    matched_files = set()
+    matched_dats = set()
+    matched_roms = {}
+    count = 0
+    for match in matches:
+        file = match[1]
+        count += 1
+        if not file.path in files:
+            continue
+        matched_files.add(file.path)
+        rom = match[0]
+        if not rom.id in matched_roms:
+            matched_roms[rom.id] = []
+        matched_dats.add(rom.game.dat.id)
+        matched_roms[rom.id].append(file.path)
 
     log.info('Calculating statistics')
-
-    matched_categories = {category_for_game[game_for_rom[rom]] for rom in matches}
-    for category in sorted(matched_categories):
+    for dat_id in matched_dats:
+        dat = session.query(rommer.Dat) \
+                         .options(joinedload(rommer.Dat.games) \
+                                 .joinedload(rommer.Game.roms)) \
+                         .filter_by(id=dat_id).first()
         have = 0
         miss = 0
-        for game in dats[category]:
+        for game in dat.games:
             for rom in game.roms:
-                if rom in matches:
+                if rom.id in matched_roms:
                     have += 1
                 else:
                     miss += 1
         if have > 0:
             total = have + miss
             percent = 100 * have / total
-            print(f'{category}: {have}/{total} ({percent}%)')
+            print(f'{dat.name}: {have}/{total} ({percent}%)')
 
-    print(f'{len(unmatched)} unmatched')
-    """
+    print(f'Unmatched: {len(files) - len(matched_files)} / {len(files)}')
