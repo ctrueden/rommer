f53e5d0b18d873959be64cf8d365d2ac03c7137d
Curtis Rueden <ctrueden@wisc.edu>
2020-12-22 13:35:32 -0600

    Rename plugins to commands
    Who knows, maybe there will be other kinds of plugins in the future.
    These particular ones are all the subcommands. Let's be specific.

--- rommer/__main__.py
+++ rommer/__main__.py
@@ -8,26 +8,26 @@ import logging
 import pkgutil
 import sys
 
-import rommer.plugins
+import rommer.commands
 
 
-def load_plugins():
-    plugins = {}
-    package = rommer.plugins
+def load_commands():
+    commands = {}
+    package = rommer.commands
     for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
         full_name = package.__name__ + '.' + name
-        plugins[name] = importlib.import_module(full_name)
-    return plugins
+        commands[name] = importlib.import_module(full_name)
+    return commands
 
 
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
 
-    plugins = load_plugins()
+    commands = load_commands()
     blurbs = []
-    for name, plugin in plugins.items():
-        desc = f' {plugin.blurb}' if hasattr(plugin, 'blurb') else ''
+    for name, command in commands.items():
+        desc = f' {command.blurb}' if hasattr(command, 'blurb') else ''
         blurbs.append(f'{name:<21}{desc}')
     blurbs.append('')
     blurbs.append('Run subcommand followed by -h flag for more detailed help.')
@@ -37,14 +37,14 @@ def main(args=None):
     parser.add_argument('-v', '--verbose', action='store_true', help='verbose mode')
     subparsers = parser.add_subparsers(dest="subcommand", description="\n".join(blurbs))
     subparsers.required = True
-    for name, plugin in plugins.items():
-        desc = f' {plugin.description}' if hasattr(plugin, 'description') else Non
-        plugin.configure(subparsers.add_parser(name, description=desc))
+    for name, command in commands.items():
+        desc = f' {command.description}' if hasattr(command, 'description') else Non
+        command.configure(subparsers.add_parser(name, description=desc))
 
     pargs = parser.parse_args(args)
     if pargs.verbose:
         logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
-    return plugins[pargs.subcommand].run(pargs)
+    return commands[pargs.subcommand].run(pargs)
 
 
 if __name__ == "__main__":
